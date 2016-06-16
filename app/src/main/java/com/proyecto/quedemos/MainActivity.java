9326c46747ee95b3a02ebdf79a554a2c445328aa
millanky <millanky@gmail.com>
2016-06-16 17:11:18 +0200

    Facebook Login

--- app/src/main/java/com/proyecto/quedemos/MainActivity.java
+++ app/src/main/java/com/proyecto/quedemos/MainActivity.java
@@ -1,24 +1,15 @@
-package com.android1.quedemos;
+package com.proyecto.quedemos;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import android.widget.TextView;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,11 +33,9 @@ public class MainActivity extends AppCompatActivity {
         if (isLogued()) {
             setContentView(R.layout.activity_main);
         } else {
+            FacebookSdk.sdkInitialize(getApplicationContext());
             setContentView(R.layout.activity_main_not_logued);
         }
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
     }
