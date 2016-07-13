d4371958e25f66626bd26740b9d94f479f39c483
millanky <millanky@gmail.com>
2016-07-13 21:12:15 +0200

    Caldroid

---
+++ app/src/main/java/com/proyecto/quedemos/CaldroidCustomAdapter.java
@@ -0,0 +1,117 @@
+package com.proyecto.quedemos;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidGridAdapter;
+
+import java.util.Map;
+
+import hirondelle.date4j.DateTime;
+
+/**
+ * Created by Usuario on 30/06/2016.
+ */
+public class CaldroidCustomAdapter extends CaldroidGridAdapter  {
+
+    public CaldroidCustomAdapter(Context context, int month, int year,
+                                       Map<String, Object> caldroidData,
+                                       Map<String, Object> extraData) {
+        super(context, month, year, caldroidData, extraData);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View cellView = convertView;
+
+        // For reuse
+        if (convertView == null) {
+            cellView = inflater.inflate(R.layout.custom_calendar_cell, null);
+        }
+
+        int topPadding = cellView.getPaddingTop();
+        int leftPadding = cellView.getPaddingLeft();
+        int bottomPadding = cellView.getPaddingBottom();
+        int rightPadding = cellView.getPaddingRight();
+
+        TextView dia = (TextView) cellView.findViewById(R.id.dia);
+        TextView eventos = (TextView) cellView.findViewById(R.id.eventos);
+
+        dia.setTextColor(Color.BLACK);
+
+        // Get dateTime of this cell
+        DateTime dateTime = this.datetimeList.get(position);
+        Resources resources = context.getResources();
+
+        // Set color of the dates in previous / next month
+        if (dateTime.getMonth() != month) {
+            dia.setTextColor(resources
+                    .getColor(com.caldroid.R.color.caldroid_darker_gray));
+        }
+
+        boolean shouldResetDiabledView = false;
+        boolean shouldResetSelectedView = false;
+
+        // Customize for disabled dates and date outside min/max dates
+        if ((minDateTime != null && dateTime.lt(minDateTime))
+                || (maxDateTime != null && dateTime.gt(maxDateTime))
+                || (disableDates != null && disableDates.indexOf(dateTime) != -1)) {
+
+            dia.setTextColor(CaldroidFragment.disabledTextColor);
+            if (CaldroidFragment.disabledBackgroundDrawable == -1) {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.disable_cell);
+            } else {
+                cellView.setBackgroundResource(CaldroidFragment.disabledBackgroundDrawable);
+            }
+
+            if (dateTime.equals(getToday())) {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.red_border_gray_bg);
+            }
+
+        } else {
+            shouldResetDiabledView = true;
+        }
+
+        // Customize for selected dates
+        if (selectedDates != null && selectedDates.indexOf(dateTime) != -1) {
+            cellView.setBackgroundColor(resources
+                    .getColor(com.caldroid.R.color.caldroid_sky_blue));
+
+            dia.setTextColor(Color.BLACK);
+
+        } else {
+            shouldResetSelectedView = true;
+        }
+
+        if (shouldResetDiabledView && shouldResetSelectedView) {
+            // Customize for today
+            if (dateTime.equals(getToday())) {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.red_border);
+            } else {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.cell_bg);
+            }
+        }
+
+        dia.setText("" + dateTime.getDay());
+        eventos.setText("Hi");
+
+        // Somehow after setBackgroundResource, the padding collapse.
+        // This is to recover the padding
+        cellView.setPadding(leftPadding, topPadding, rightPadding,
+                bottomPadding);
+
+        // Set custom color if required
+        setCustomResources(dateTime, cellView, dia);
+
+        return cellView;
+    }
+
+}
